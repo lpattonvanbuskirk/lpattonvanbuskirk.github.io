754ea36c3858d19964c18916d09589a2cd51aa54
Luke Patton-Van Buskirk <84169622+lpattonvanbuskirk@users.noreply.github.com>
2026-03-11 15:48:15 -0500

    Update script.js
    terrible debugging practice test

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ async function loadAssets() {
   const container = document.getElementById("content");
 
   const files = await fetch("assets/files.json").then(r => r.json());
-
+  console.log(files);
   for (const file of files) {
     const text = await fetch(`assets/${file}`).then(r => r.text());
-
+    
     const pre = document.createElement("pre");
     pre.textContent = text;
+
+    console.log(text);
 
     container.appendChild(pre);
   }
